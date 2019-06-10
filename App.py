b1b2296c061854f705c95a82c01805d45d0b81f6
Braiunito <braiantablet@gmail.com>
2019-06-10 13:13:09 -0300

    Flash y flashed (MENSAJES)

--- App.py
+++ App.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 #Codio para iniciar el servidor
 app=Flask(__name__)
+
+#MySql Connection
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='42922075'
@@ -14,6 +16,10 @@ mysql= MySQL(app)
 def Index():
 	return render_template('index.html')
 
+
+#Sessions/Settings
+app.secret_key = 'mysecretkey'
+
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
 	if request.method == 'POST':
@@ -24,7 +30,8 @@ def add_contact():
 		cur = mysql.connection.cursor()
 		cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)',(fullname, phone, email))
 		mysql.connection.commit()
-	return 'Recived'
+		flash('Contact Added syccesfully')
+	return redirect(url_for('index'))
 
 @app.route('/edit')
 def edit_contact():
